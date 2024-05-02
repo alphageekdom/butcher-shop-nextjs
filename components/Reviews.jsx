4efdeab72ae6c27a72d37b823ea9981fa3aa27d4
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-05-02 09:01:41 -0700

    Favorites UX/UI

--- components/Reviews.jsx
+++ components/Reviews.jsx
@@ -1,7 +1,6 @@
 import ReviewCard from './ReviewCard';
 import JosephImage from '@/assets/images/joseph.jpg';
 import SoniaImage from '@/assets/images/sonia.jpg';
-import AlbertImage from '@/assets/images/albert.jpg';
 
 const Reviews = () => {
   return (
@@ -10,27 +9,22 @@ const Reviews = () => {
         <h1 className='text-4xl font-bold text-black mb-6 text-center'>
           Customer Reviews
         </h1>
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
           <ReviewCard
+            bgColor='white'
+            text='black'
             name='Joseph Doe'
-            date='May 1, 2022'
             review="Absolutely top-notch meat cuts! Every bite is a testament to the quality and expertise of EliteCuts. As a local in Southern California, I'm fortunate to have such a gem nearby."
             imageSrc={JosephImage}
-            favoriteMeatCut='/products/6632a88bdfbba9d57372e8a8'
+            favorite='/products/6632a88bdfbba9d57372e8a8'
           />
           <ReviewCard
+            bgColor='black'
+            text='white'
             name='Sonia Smith'
-            date='April 28, 2020'
             review="EliteCuts' meat cuts are unparalleled. Each piece is tender, succulent, and elevates any meal to a gourmet experience. Living in Southern California, I can't imagine going anywhere else for my meat."
             imageSrc={SoniaImage}
-            favoriteMeatCut='/products/6632a88bdfbba9d57372e8b3'
-          />
-          <ReviewCard
-            name='Albert Johnson'
-            date='April 25, 2024'
-            review="I'm consistently amazed by the caliber of meat cuts from EliteCuts. Their commitment to excellence shines through in every bite! Being in Southern California, EliteCuts is my go-to for premium meats."
-            imageSrc={AlbertImage}
-            favoriteMeatCut='/products/6632a88bdfbba9d57372e8b4'
+            favorite='/products/6632a88bdfbba9d57372e8b3'
           />
         </div>
       </div>
