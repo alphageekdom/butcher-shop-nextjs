f4b846f77522a249662f0588ae32a61cae13ee40
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-05-18 11:40:30 -0700

    another push

--- app/products/saved/page.jsx
+++ app/products/saved/page.jsx
@@ -1,31 +1,44 @@
+'use client';
+import { useState, useEffect, useCallback } from 'react';
 import SavedProducts from '@/components/SavedProducts';
-import connectDB from '@/config/database';
-import User from '@/models/User';
-import { getSessionUser } from '@/utils/getSessionUser';
-import { convertToSerializeableObject } from '@/utils/convertToObject';
 
-const SavedProductsPage = async () => {
-  await connectDB();
+const SavedProductsPage = () => {
+  const [bookmarks, setBookmarks] = useState([]);
 
-  const sessionUser = await getSessionUser();
+  const fetchBookmarks = useCallback(async () => {
+    try {
+      const res = await fetch('/api/bookmarks', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
 
-  const { userId } = sessionUser;
+      if (res.ok) {
+        const data = await res.json();
+        setBookmarks(data);
+      } else {
+        console.error('Failed to fetch new bookmarks');
+      }
+    } catch (error) {
+      console.error('Error fetching bookmarks:', error);
+    }
+  }, []);
 
-  const user = await User.findById(userId).populate('bookmarks').lean();
+  useEffect(() => {
+    fetchBookmarks();
+  }, [fetchBookmarks]);
 
-  const bookmarks = user?.bookmarks || [];
-
-  const plainBookmarks = bookmarks.map((bookmark) =>
-    convertToSerializeableObject(bookmark)
-  );
+  console.log(bookmarks);
 
   return (
     <section className='px-4 py-6'>
       <div className='container-xl lg:container m-auto px-4 py-6'>
         <h1 className='text-2xl mb-4'>Saved Cuts</h1>
-        <SavedProducts initialBookmarks={plainBookmarks} />
+        <SavedProducts bookmarks={bookmarks} />
       </div>
     </section>
   );
 };
+
 export default SavedProductsPage;
--- components/SavedProducts.jsx
+++ components/SavedProducts.jsx
@@ -1,25 +1,6 @@
-'use client';
-
-import { useState, useCallback } from 'react';
 import ProductCard from './product/ProductCard';
 
-const SavedProducts = ({ initialBookmarks }) => {
-  const [bookmarks, setBookmarks] = useState(initialBookmarks);
-
-  const handleBookmarkChange = useCallback(async () => {
-    try {
-      const res = await fetch('/api/bookmarks');
-      if (res.ok) {
-        const newBookmarks = await res.json();
-        setBookmarks(newBookmarks);
-      } else {
-        console.error('Failed to fetch new bookmarks');
-      }
-    } catch (error) {
-      console.error('Error fetching bookmarks:', error);
-    }
-  }, []);
-
+const SavedProducts = ({ bookmarks }) => {
   if (!bookmarks || bookmarks.length === 0) {
     return <p>No cuts found.</p>;
   }
@@ -27,11 +8,7 @@ const SavedProducts = ({ initialBookmarks }) => {
   return (
     <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
       {bookmarks.map((product) => (
-        <ProductCard
-          key={product._id}
-          product={product}
-          onBookmarkChange={handleBookmarkChange}
-        />
+        <ProductCard key={product._id} product={product} />
       ))}
     </div>
   );
